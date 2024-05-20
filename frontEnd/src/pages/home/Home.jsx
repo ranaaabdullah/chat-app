ae4d1afe8f3f641ff782c35729e0d18ba1dcae92
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-05-20 16:42:04 +0500

    authenticcation completed

--- frontEnd/src/pages/home/Home.jsx
+++ frontEnd/src/pages/home/Home.jsx
@@ -4,12 +4,9 @@ import Login from "../login/Login";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center  h-screen   ">
-      <div className= " flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <Sidebar />
-        <MessageContainer />
-      </div>
-      {/* <Login/> */}
+    <div className=" flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      <MessageContainer />
     </div>
   );
 };
