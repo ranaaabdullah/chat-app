be00fb8b2b912798897fc8a07bd839825f2a83cc
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-05-21 20:00:45 +0500

    Funtionailty completed

--- frontEnd/src/components/messages/MessageContainer.jsx
+++ frontEnd/src/components/messages/MessageContainer.jsx
@@ -1,19 +1,27 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
-	const noChatSelected = true;
+	const { selectedConversation, setSelectedConversation } = useConversation();
+
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
+
 	return (
 		<div className='md:min-w-[450px] flex flex-col'>
-			{noChatSelected ? (
+			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
 					{/* Header */}
 					<div className='bg-slate-500 px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
-						<span className='text-gray-900 font-bold'>John doe</span>
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 					</div>
 					<Messages />
 					<MessageInput />
@@ -33,5 +41,5 @@ const NoChatSelected = () => {
 				<TiMessages className='text-3xl md:text-6xl text-center' />
 			</div>
 		</div>
-	);
+	);``
 };
